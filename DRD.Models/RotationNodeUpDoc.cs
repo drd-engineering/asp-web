9754e855faff2aec34a231ea6db6d4dddb6d327c
frannadrd <franna@drdaccess.com>
2019-11-04 12:49:35 +0700

    [ADD] orm

---
+++ DRD.Models/RotationNodeUpDoc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRD.Domain
+{
+    public class RotationNodeUpDoc
+    {
+        public long Id { get; set; } // Id (Primary key)
+        public long? DocumentUploadId { get; set; } // DocumentUploadId
+
+        // Foreign keys
+        public virtual DocumentUpload DocumentUpload { get; set; } // FK_RotationNodeUpDoc_DocumentUpload
+        public virtual RotationNode RotationNode { get; set; } // FK_RotationNodeUpDoc_RotationNode
+
+        public RotationNodeUpDoc()
+        {
+        }
+    }
+}
