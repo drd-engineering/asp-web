e7467d54b9690533206d11ddb4c8b303765ef95e
Bagaskoro Putra <bagas@drdaccess.com>
2019-11-04 19:52:30 +0700

    List all Models to MyDBContext

--- DRD.Core/Models/MyDBContext.cs
+++ DRD.Core/Models/MyDBContext.cs
@@ -14,8 +14,27 @@ namespace DRD.Core.Models
         {
 
         }
-        public DbSet<AnnotateType> annotateTypes { get; set; } 
-        public DbSet<Company> companies { get; set; }
-        
+        public DbSet<AnnotateType> AnnotateType { get; set; } 
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Document> Document { get; set; }
+        public DbSet<DocumentElement> DocumentElement { get; set; }
+        public DbSet<DocumentSign> DocumentSign { get; set; }
+        public DbSet<FaspayData> FaspayData { get; set; }
+        public DbSet<Member> Member { get; set; }
+         public DbSet<Plan> Plan { get; set; }
+        public DbSet<Rotation> Rotation { get; set; }
+        public DbSet<RotationMember> RotationMember { get; set; }
+        public DbSet<RotationNode> RotationNode { get; set; }
+        public DbSet<RotationNodeDoc> RotationNodeDoc { get; set; }
+        public DbSet<RotationNodeRemark> RotationNodeRemark { get; set; }
+        public DbSet<RotationNodeUpDoc> RotationNodeUpDoc { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserAdmin> UserAdmin { get; set; }
+        public DbSet<Workflow> Workflow { get; set; }
+        public DbSet<WorkflowNode> WorkflowNode { get; set; }
+        public DbSet<WorkflowNodeLink> WorkflowNodeLink { get; set; }
+
+
+
     }
 }
--- DRD.Models/Plan.cs
+++ DRD.Models/Plan.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DRD.Domain
+namespace DRD.Models
+
 {
     public class Plan
     {
