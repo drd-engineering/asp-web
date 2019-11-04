ecc1fbf188dbc735b4370d4e6ac3fedc82bcf8f4
Rani Lasma Uli <rani@drdaccess.com>
2019-11-04 12:52:57 +0700

    remove master attribute

---
+++ DRD.Models/MemberFolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRD.Domain
+{
+    public class MemberFolder
+    {
+        public long Id { get; set; } // Id (Primary key)
+        public long MemberId { get; set; } // MemberId
+        public string Name { get; set; } // Name (length: 50)
+        public string Description { get; set; } // Descr (length: 250)
+        public int FolderType { get; set; } // FolderType
+        public System.DateTime DateCreated { get; set; } // DateCreated
+        public System.DateTime? DateUpdated { get; set; } // DateUpdated
+    }
+}
