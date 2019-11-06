6c8b93b0468b361d0643a51a2674e2b0f8448296
Rani Lasma Uli <rani@drdaccess.com>
2019-11-06 13:06:45 +0700

    add document in drdcontext

--- Core.Postgres/DRDContext.cs
+++ Core.Postgres/DRDContext.cs
@@ -15,22 +15,23 @@ namespace Core.Postgres
         public DbSet<ElementType> ElementTypes { get; set; }
 
         public DbSet<Company> Companies { get; set; }
-       // public DbSet<Document> Documents { get; set; }
+
+        public DbSet<Document> Documents { get; set; }
        // public DbSet<DocumentElement> DocumentElements { get; set; }
        // public DbSet<DocumentSign> DocumentSigns { get; set; }
        // public DbSet<FaspayData> FaspayDatas { get; set; }
         public DbSet<Member> Members { get; set; }
         public DbSet<Plan> Plans { get; set; }
-       // public DbSet<Rotation> Rotations { get; set; }
-       // public DbSet<RotationMember> RotationMembers { get; set; }
-       // public DbSet<RotationNode> RotationNodes { get; set; }
-       // public DbSet<RotationNodeDoc> RotationNodeDocs { get; set; }
-       // public DbSet<RotationNodeRemark> RotationNodeRemarks { get; set; }
-       // public DbSet<RotationNodeUpDoc> RotationNodeUpDocs { get; set; }
+        // public DbSet<Rotation> Rotations { get; set; }
+        // public DbSet<RotationMember> RotationMembers { get; set; }
+        // public DbSet<RotationNode> RotationNodes { get; set; }
+        // public DbSet<RotationNodeDoc> RotationNodeDocs { get; set; }
+        // public DbSet<RotationNodeRemark> RotationNodeRemarks { get; set; }
+        // public DbSet<RotationNodeUpDoc> RotationNodeUpDocs { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserAdmin> UserAdmins { get; set; }
-       // public DbSet<Workflow> Workflows { get; set; }
-       // public DbSet<WorkflowNode> WorkflowNodes { get; set; }
-       // public DbSet<WorkflowNodeLink> WorkflowNodeLinks { get; set; }
+        // public DbSet<Workflow> Workflows { get; set; }
+        // public DbSet<WorkflowNode> WorkflowNodes { get; set; }
+        // public DbSet<WorkflowNodeLink> WorkflowNodeLinks { get; set; }
     }
 }
