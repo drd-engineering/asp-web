e78d6939097a60576d2b11dcd1a60cf370ce0a9d
Rani Lasma Uli <rani@drdaccess.com>
2019-11-06 13:09:55 +0700

    add workflow to db

--- Core.Postgres/DRDContext.cs
+++ Core.Postgres/DRDContext.cs
@@ -30,7 +30,7 @@ namespace Core.Postgres
         // public DbSet<RotationNodeUpDoc> RotationNodeUpDocs { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserAdmin> UserAdmins { get; set; }
-        // public DbSet<Workflow> Workflows { get; set; }
+        public DbSet<Workflow> Workflows { get; set; }
         // public DbSet<WorkflowNode> WorkflowNodes { get; set; }
         // public DbSet<WorkflowNodeLink> WorkflowNodeLinks { get; set; }
     }
---
+++ Core.Postgres/Models/Workflow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Postgres.Models
+{
+    public class Workflow
+    {
+        public long Id { get; set; } // Id (Primary key)
+        public string Name { get; set; } // Name (length: 100)
+        public string Descr { get; set; } // Descr
+        public bool IsActive { get; set; } // IsActive
+        public long? CreatorId { get; set; } // CreatorId
+        public bool IsTemplate { get; set; } // IsTemplate
+        public int WfType { get; set; } // WfType
+        public string UserId { get; set; } // UserId (length: 50)
+        public System.DateTime DateCreated { get; set; } // DateCreated
+        public System.DateTime? DateUpdated { get; set; } // DateUpdated
+
+        // Reverse navigation
+        // public virtual System.Collections.Generic.ICollection<WorkflowNode> WorkflowNodes { get; set; } // WorkflowNode.FK_WorkflowNode_Workflow
+
+        public Workflow()
+        {
+            // WorkflowNodes = new System.Collections.Generic.List<WorkflowNode>();
+            WfType = 0;
+        }
+    }
+}
