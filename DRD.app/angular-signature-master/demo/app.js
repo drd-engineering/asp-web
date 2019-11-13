c0e92773045970fb8c1afeb639decfe716fac113
fran na <franna@drdaccess.com>
2019-11-13 14:18:39 +0700

    [RESET] delete DRD.app and DRD.App, repair project structure

--- DRD.app/angular-signature-master/demo/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('app', ['signature']);
-
-angular.module('app').controller('AppCtrl', function($scope) {
-    $scope.boundingBox = {
-        width: 700,
-        height: 300
-    };
-});
-
