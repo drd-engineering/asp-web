8191366181457fe01228321ced7d7eb38abd3750
fran na <franna@drdaccess.com>
2019-11-14 13:39:17 +0700

    [CHANGE] adding seed data of user and company

--- Core.Postgres/DRDContext.cs
+++ Core.Postgres/DRDContext.cs
@@ -33,5 +33,37 @@ namespace Core.Postgres
         public DbSet<Workflow> Workflows { get; set; }
         // public DbSet<WorkflowNode> WorkflowNodes { get; set; }
         // public DbSet<WorkflowNodeLink> WorkflowNodeLinks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            List < User > listOfUserCreated = new List<User>();
+            listOfUserCreated.Add(new User { Id = 11111111, Name = "aminudin bin saleh", Phone = "085140451404", Email = "aminudin.saleh@hotmail.com",
+                OfficialIdNo = 3511101010101010, ImageProfile = "hehehehe.png", Password = "hXKFKEfd!!jafIenJ@42FDNKDjdaknfj",
+                ImageSignature = null, ImageInitials = null, ImageStamp = null, ImageKtp1 = null, ImageKtp2 = null,
+                IsActive = true, CreatedAt = DateTime.Now
+            });
+            listOfUserCreated.Add(new User { Id = 11111121, Name = "amanudin bin saleh", Phone = "085858585858", Email = "amanudin.saleh@hotmail.com",
+                OfficialIdNo = 3511202020202020, ImageProfile = "hahahaha.png", Password = "hX83KEfd!!jaJFKnJ@42FDNKDjKDLnfj",
+                ImageSignature = null, ImageInitials = null, ImageStamp = null, ImageKtp1 = null, ImageKtp2 = null,
+                IsActive = true, CreatedAt = DateTime.Now
+            });
+            listOfUserCreated.Add(new User { Id = 11111211, Name = "amirudin bin saleh", Phone = "085151515151", Email = "amirudin.saleh@hotmail.com",
+                OfficialIdNo = 3511303030303030, ImageProfile = "huhuhuhu.png", Password = "hXk2O1fd!!jafIJpw)92FDNKDjdaknfj",
+                ImageSignature = null, ImageInitials = null, ImageStamp = null, ImageKtp1 = null, ImageKtp2 = null,
+                IsActive = true, CreatedAt = DateTime.Now
+            });
+            listOfUserCreated.Add(new User { Id = 11112111, Name = "amiradin bin saleh", Phone = "085888811111", Email = "amiradin.saleh@hotmail.com",
+                OfficialIdNo = 3511404040404040, ImageProfile = "hohohoho.png", Password = "hLoe90fd!!jaJFKnJ@42FDnf@3kDLnfj",
+                ImageSignature = null, ImageInitials = null, ImageStamp = null, ImageKtp1 = null, ImageKtp2 = null,
+                IsActive = true, CreatedAt = DateTime.Now
+            });
+            modelBuilder.Entity<User>().HasData( listOfUserCreated[0], listOfUserCreated[1], listOfUserCreated[2], listOfUserCreated[3] );
+
+            List<Company> listOfCompanyCreated = new List<Company>();
+            listOfCompanyCreated.Add(
+                new Company{Id = 1111112, Code = "DG23JJKL3L", Name = "PT SEMPOA", Contact = Phone Email Descr Address PointLocation PostalCode Image1 Image2 ImageCard IsActive UserId
+                CreatedAt  ICollection<Member> Members Tags});
+
+            modelBuilder.Entity<Company>().HasData( );
+        }
     }
 }
