137033cda0d76d2857c0b347e7cfc70ddfa09562
fran na <franna@drdaccess.com>
2019-11-14 10:42:13 +0700

    [CHANGE] table definition

--- DRD.Models/Company.cs
+++ DRD.Models/Company.cs
@@ -22,11 +22,15 @@ namespace DRD.Models
         public string Image2 { get; set; } // Image2 (length: 100)
         public string ImageCard { get; set; } // ImageCard (length: 100)
         public bool IsActive { get; set; } // IsActive
-        public string UserId { get; set; } // UserId (length: 50)
+        
         public DateTime CreatedAt { get; set; } // DateCreated
 
         // Reverse navigation
-       // public virtual ICollection<Document> Documents { get; set; } // Document.FK_Document_Company
+
+        // FK User as owner
+        public User Owner{ get; set; } // UserId (length: 50)
+
+        // public virtual ICollection<Document> Documents { get; set; } // Document.FK_Document_Company
         public virtual ICollection<Member> Members { get; set; } // Member.FK_Member_Company
         //public virtual System.Collections.Generic.ICollection<DtoMemberSubscribe> MemberSubscribes { get; set; } // MemberSubscribe.FK_MemberSubscribe_Company
         public virtual ICollection<Tag> Tags { get; set; } // Project.FK_Project_Company
@@ -37,8 +41,8 @@ namespace DRD.Models
         public Company()
         {
             IsActive = true;
-            UserId = "SYST";
             CreatedAt = DateTime.Now;
+            Owner = new User();
             Members = new List<Member>();
             Tags = new List<Tag>();
         }
