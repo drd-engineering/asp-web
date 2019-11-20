469757ec90c266fb78db0dc6eb7509b679e287d3
fran na <franna@drdaccess.com>
2019-11-20 18:49:09 +0700

    [ADD] dashboard service

---
+++ DRD.Models/View/Dashboard/CounterItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRD.Models.View.Dashboard
+{
+    public class CounterItem
+    {
+        public Item Old { get; set; }
+        public Item New { get; set; }
+
+        public CounterItem()
+        {
+            Old = new Item();
+            New = new Item();
+        }
+
+        public class Item
+        {
+            public int Rotation { get; set; }
+            public int Inbox { get; set; }
+            public int Altered { get; set; }
+            public int Revised { get; set; }
+            public int InProgress { get; set; }
+            public int Pending { get; set; }
+            public int Signed { get; set; }
+            public int Declined { get; set; }
+            public int Completed { get; set; }
+            public int Contact { get; set; }
+            public int Document { get; set; }
+        }
+    }
+}
+
+
