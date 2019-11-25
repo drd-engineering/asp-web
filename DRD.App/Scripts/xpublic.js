e19f0d9d27c402f82e76e069b7b7b22099a351de
fran na <franna@drdaccess.com>
2019-11-25 13:49:09 +0700

    [ADD] add DRD.App

---
+++ DRD.App/Scripts/xpublic.js
@@ -0,0 +1,27 @@
+﻿/*--------------------------------------------------------------
+            SAWN
+--------------------------------------------------------------*/
+function showError(msg) {
+    swal({
+        title: "Error",
+        text: msg,
+        confirmButtonColor: "#2196F3",
+        type: "error"
+    });
+}
+function showSuccess(msg) {
+    swal({
+        title: "Success",
+        text: msg,
+        confirmButtonColor: "#66BB6A",
+        type: "success"
+    });
+}
+function showInfo(msg) {
+    swal({
+        title: "Information",
+        text: msg,
+        confirmButtonColor: "#66BB6A",
+        type: "info"
+    });
+}
