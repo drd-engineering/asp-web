24d66223789fd191797c5906f7fdd31189f58f2c
rani <rani@drdaccess.com>
2019-12-03 04:09:34 +0700

    add service for contact page data

--- DRD.Models/Member.cs
+++ DRD.Models/Member.cs
@@ -7,8 +7,8 @@ namespace DRD.Models
     public class Member
     {
         public long Id { get; set; } // Id (Primary key)
-        public long? CompanyId { get; set; } // CompanyId
-        public long? UserId { get; set; } // UserId (length: 50)
+        public long CompanyId { get; set; } // CompanyId
+        public long UserId { get; set; } // UserId (length: 50)
         public System.DateTime JoinedAt { get; set; } // DateCreated
         public bool IsActive { get; set; } // IsActive
         public bool IsAdministrator { get; set; } // IsActive
