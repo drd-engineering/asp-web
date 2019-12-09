3e9f7b31faaa84ac5960665de604ea474055651d
Bagaskoro Putra <bagas@drdaccess.com>
2019-12-09 21:22:22 +0700

    fix build error

---
+++ DRD.Models/View/CompanyDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRD.Models.View.Company
+{
+    public class CompanyDetail
+    {
+        public long Id { get; set; } // Id (Primary key)
+        public string Code { get; set; } // Code (length: 10)
+        public string Name { get; set; } // Name (length: 50)
+        public long TotalMember { get; set; }
+        public int limitAdmin { get; set; }
+        public long OwnerId { get; set; }
+        public bool isVerified { get; set; }
+        public string Phone { get; set; } // Phone (length: 20)
+        public string Address { get; set; } // Address (length: 1000)
+        public string PointLocation { get; set; } // PointLocation (length: 1000)
+        public List<Member> Members { get; set; }
+        public List<Member> Administrators { get; set; }
+        public CompanyDetail()
+        {
+            Members = new List<Member>();
+            Administrators = new List<Member>();
+        }
+    }
+}
