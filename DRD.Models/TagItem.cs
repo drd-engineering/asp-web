9516a74ce13ed0829ce6084825b633a8ce6af457
fran na <franna@drdaccess.com>
2020-03-12 12:03:52 +0700

    [ADD] tag system in db

---
+++ DRD.Models/TagItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DRD.Models
+{
+    [Table("TagItems", Schema = "public")]
+    public class TagItem
+    {
+        public int TagId { get; set; } 
+        public string RotationId { get; set; }
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { get; set; }
+        [ForeignKey("Rotation")]
+        public virtual Rotation Rotation { get; set; }
+    }
+}
