11b657eaa49dc74a3ec4d6c95445606602b5ecba
Bagas Meyca <bagas@drdaccess.com>
2020-05-01 17:06:39 +0700

    [FIX] db typo

--- DRD.Models/BusinessPackage.cs
+++ DRD.Models/BusinessPackage.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DRD.Models
 {
-    [Table("BusinessPacakges", Schema = "public")]
+    [Table("BusinessPackages", Schema = "public")]
     public class BusinessPackage
     {
         [Key]
