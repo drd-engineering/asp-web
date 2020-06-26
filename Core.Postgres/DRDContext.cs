286e784a98a437c4980dcd2c94adbe0f3d90b925
Bagas Meyca <bagas@drdaccess.com>
2020-06-26 19:11:00 +0700

    [DELETE]

--- Core.Postgres/DRDContext.cs
+++ Core.Postgres/DRDContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core.Postgres
 {
@@ -32,6 +34,28 @@ namespace Core.Postgres
         public DbSet<WorkflowNode> WorkflowNodes { get; set; }
         public DbSet<WorkflowNodeLink> WorkflowNodeLinks { get; set; }
 
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entity in entities)
+            {
+                var now = DateTime.UtcNow; // current datetime
+
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).CreatedAt = now;
+                }
+                ((BaseEntity)entity.Entity).UpdatedAt = now;
+            }
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             List<User> listOfUserCreated = new List<User>();
